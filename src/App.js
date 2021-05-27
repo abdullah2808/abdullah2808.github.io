553de4a8bb6203fb106b4b75e812f8cbc421e5b1
Abdullah Ahmad <61104635+abdullah2808@users.noreply.github.com>
2021-05-26 22:17:20 -0500

    Implemented Multiple pages using React Router

--- src/App.js
+++ src/App.js
@@ -4,22 +4,62 @@ import MainContent from './Components/MainContent.js'
 import ContactForm from './Components/ContactForm.js'
 import Projects from './Components/Projects.js'
 import AboutMe from './Components/AboutMe.js'
-
+import Pdf from './Components/Resume.pdf';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 class App extends React.Component
 {
-  render(){
-  return (
-    <div className = "bg-gray-100">
-      <Navbar />
-      <div> <AboutMe/> </div>
-      <div classname = "rounded-b-lg"> <MainContent/> </div>
-      <div className = 'bg-gray-100 mt-10'> <Projects/> </div>
-      <div className ='bg-gray-600'> <div className = 'mt-20 p-10'> <ContactForm/> </div></div>
-    </div>
-    
+    render(){
+    return(
+        <Router className>
+        <div className = "bg-gray-100">
+        <div className = " bg-pink-400 p-5 font-mono text-lg">
+            <nav className = "flex justify-between " >
+                <div>
+                    <ul className = 'flex flex-rows'>
+                        <li><Link to = "/" className = "block px-4 py-2 text-md font-extrabold text-gray-700 rounded-md hover:bg-white"> Home         </Link></li>
+                        <li><Link to = '/Projects' className = "block px-4 py-2 text-md font-extrabold text-gray-700 rounded-md hover:bg-white"> Projects     </Link> </li>
+                        <li><a href = 'https://github.com/abdullah2808/' className = "block px-4 py-2 text-md font-extrabold text-gray-700 rounded-md hover:bg-white" target="_blank"> GitHub    </a></li>
+                        <li><a href = {Pdf} className = "block px-4 py-2 text-md font-extrabold text-gray-700 rounded-md hover:bg-white" target="_blank"> Resume       </a></li>
+                    </ul>
+                </div>
 
-  )
-  }
+                <div> 
+                    <a href = '/ContactForm' className = "block px-4 py-2 text-md font-extrabold text-gray-700 rounded-md hover:bg-white"> Contact Me   </a>
+                </div>
+                
+            </nav>
+            </div>
+            </div>
+            <Switch>
+          
+          <Route path="/Projects">
+          <div className = "bg-gray-100 pt-10">
+          <Projects/> 
+          </div>
+          </Route>
+          <Route path ="/ContactForm">
+          <div  className = "bg-gray-600" >
+          <ContactForm/>
+          </div>
+          </Route>
+          <Route path="/">
+          <div className = "bg-gray-100">
+            <div> <AboutMe/> </div>
+            <div classname = "rounded-b-lg"> <MainContent/> </div>
+            <div className = 'bg-gray-100 mt-10'> <Projects/> </div>
+            <div className ='bg-gray-600'> <div className = 'mt-20 p-10'> <ContactForm/> </div></div>
+            </div>
+          </Route>
+        </Switch>
+        </Router>
+    )
+    }
 }
+
 
 export default App;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,17 +1,29 @@
 import { render } from '@testing-library/react'
 import React from 'react'
 import Pdf from './Resume.pdf';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+import MainContent from './MainContent.js';
+import ContactForm from './ContactForm.js';
+import Projects from './Projects.js';
+import AboutMe from './AboutMe.js';
 
 class Navbar extends React.Component{
     render(){
     return(
+        <Router>
+        <div className = "bg-gray-100">
         <div className = " bg-pink-400 p-5 font-mono text-lg">
             <nav className = "flex justify-between " >
                 <div>
                     <ul className = 'flex flex-rows'>
-                        <li><a href = "#" className = "block px-4 py-2 text-md font-extrabold text-gray-700 rounded-md hover:bg-white"> Home         </a></li>
-                        <li><a href = '#' className = "block px-4 py-2 text-md font-extrabold text-gray-700 rounded-md hover:bg-white"> Projects     </a> </li>
-                        <li><a href = 'https://github.com/abdullah2808/' className = "block px-4 py-2 text-md font-extrabold text-gray-700 rounded-md hover:bg-white" target="_blank"> GitHub       </a></li>
+                        <li><Link to = "/" className = "block px-4 py-2 text-md font-extrabold text-gray-700 rounded-md hover:bg-white"> Home         </Link></li>
+                        <li><Link to = '/Projects' className = "block px-4 py-2 text-md font-extrabold text-gray-700 rounded-md hover:bg-white"> Projects     </Link> </li>
+                        <li><a href = 'https://github.com/abdullah2808/' className = "block px-4 py-2 text-md font-extrabold text-gray-700 rounded-md hover:bg-white" target="_blank"> GitHub    </a></li>
                         <li><a href = {Pdf} className = "block px-4 py-2 text-md font-extrabold text-gray-700 rounded-md hover:bg-white" target="_blank"> Resume       </a></li>
                     </ul>
                 </div>
@@ -20,7 +32,25 @@ class Navbar extends React.Component{
                     <a href = '#' className = "block px-4 py-2 text-md font-extrabold text-gray-700 rounded-md hover:bg-white"> Contact Me   </a>
                 </div>
             </nav>
+            </div>
+            <Switch>
+          <Route path="/">
+          <div className = "bg-gray-100">
+            <div> <AboutMe/> </div>
+            <div classname = "rounded-b-lg"> <MainContent/> </div>
+            <div className = 'bg-gray-100 mt-10'> <Projects/> </div>
+            <div className ='bg-gray-600'> <div className = 'mt-20 p-10'> <ContactForm/> </div></div>
+            </div>
+          </Route>
+          <Route path="/Projects">
+          <div className = "bg-gray-100">
+            <div className = 'bg-gray-100 mt-10'> <Projects/> </div>
+            </div>
+          </Route>
+        </Switch>
+
         </div>
+        </Router>
     )
     }
 }
